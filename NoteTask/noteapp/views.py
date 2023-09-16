709b81b294984e9a0a1743461f8f35736ffe6fcf
shubhamvisare22 <shubhamvisare22@gmail.com>
2023-09-16 18:17:12 +0530

    updated user spicific notes

--- NoteTask/noteapp/views.py
+++ NoteTask/noteapp/views.py
@@ -1,18 +1,20 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view ,permission_classes
-from rest_framework import status,permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status, permissions
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
-# custom imports 
+# custom imports
 from .serializers import NoteSerilizer
 from .models import NoteModel
 from .paginations import CustomPagination
 
 '''---------- User Related views --------'''
+
+
 @api_view(['POST'])
-@permission_classes([permissions.AllowAny]) 
+@permission_classes([permissions.AllowAny])
 def register(request):
     if request.method == 'POST':
         username = request.data.get('username')
@@ -25,7 +27,8 @@ def register(request):
             return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
 
         user = User.objects.create_user(username=username, password=password)
-        token, _ = Token.objects.get_or_create(user=user)  # Generate a token for the user
+        # Generate a token for the user
+        token, _ = Token.objects.get_or_create(user=user)
 
         return Response({'token': token.key}, status=status.HTTP_201_CREATED)
     return Response({'error': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
@@ -33,90 +36,89 @@ def register(request):
 
 '''-------- Notes Related Views --------'''
 
+
 def sort_notes(queryset, sort_param):
     if sort_param not in ['created_at', 'updated_at']:
         return queryset
-    return queryset.order_by(f'-{sort_param}') # recently updated or created
+    return queryset.order_by(f'-{sort_param}')  # recently updated or created
 
 # Create Notes
+
+
 @api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated]) 
+@permission_classes([permissions.IsAuthenticated])
 def create_note(request):
     serializer = NoteSerilizer(data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
-        return Response({'msg':'Note created successfully.'}, status=status.HTTP_200_OK)
+        return Response({'msg': 'Note created successfully.'}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # List Note
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated]) 
+@permission_classes([permissions.IsAuthenticated])
 def list_notes(request):
-    if request.method == 'GET': 
-        
+    if request.method == 'GET':
+
         # sorting on update_at or created_at
-        sort_param = request.GET.get('sort', 'created_at') # by default sort by creation time
-        note_objs = NoteModel.objects.all()
+        # by default sort by creation time
+        sort_param = request.GET.get('sort', 'created_at')
+        note_objs = NoteModel.objects.filter(user=request.user)
         sorted_note_objs = sort_notes(note_objs, sort_param)
 
         # pagination here
         paginator = CustomPagination()
         result_pages = paginator.paginate_queryset(sorted_note_objs, request)
         serializer = NoteSerilizer(result_pages, many=True)
-        
+
         msg = 'All Note fetched successfully.'
         if serializer.data == []:
             msg = 'No Notes Found!'
-            
-        return paginator.get_paginated_response({'msg':msg, 'notes':serializer.data})
-    return Response({'msg': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
 
+        return paginator.get_paginated_response({'msg': msg, 'notes': serializer.data})
+    return Response({'msg': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Retrieve Note
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated]) 
+@permission_classes([permissions.IsAuthenticated])
 def retrieve_note(request, id):
     if request.method == 'GET':
-            try:
-                retrive_obj  = NoteModel.objects.get(id=id)
-                serializer = NoteSerilizer(retrive_obj)
-                return Response({'msg':' Note fetched successfully.', 'notes':serializer.data}, status=status.HTTP_200_OK)
-            except NoteModel.DoesNotExist:
-                return Response({'msg':'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'msg':'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
- 
-            
+        try:
+            retrive_obj = NoteModel.objects.get(id=id, user=request.user)
+            serializer = NoteSerilizer(retrive_obj)
+            return Response({'msg': ' Note fetched successfully.', 'notes': serializer.data}, status=status.HTTP_200_OK)
+        except NoteModel.DoesNotExist:
+            return Response({'msg': 'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'msg': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Update Note
 @api_view(['PUT'])
-@permission_classes([permissions.IsAuthenticated]) 
+@permission_classes([permissions.IsAuthenticated])
 def update_note(request, id):
     if request.method == 'PUT':
         try:
-            retrive_obj  = NoteModel.objects.get(id=id)
-            serializer = NoteSerilizer(retrive_obj, data = request.data)
-            if  serializer.is_valid():
+            retrive_obj = NoteModel.objects.get(id=id, user=request.user)
+            serializer = NoteSerilizer(retrive_obj, data=request.data)
+            if serializer.is_valid():
                 serializer.save(request.user)
-                return Response({'msg':' Note Updated successfully.', 'notes':serializer.data}, status=status.HTTP_200_OK)
+                return Response({'msg': ' Note Updated successfully.', 'notes': serializer.data}, status=status.HTTP_200_OK)
         except NoteModel.DoesNotExist:
-            return Response({'msg':'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)    
-    return Response({'msg':'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    
-    
+            return Response({'msg': 'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'msg': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
+
+
 # Delete Note
 @api_view(['DELETE'])
-@permission_classes([permissions.IsAuthenticated]) 
+@permission_classes([permissions.IsAuthenticated])
 def delete_note(request, id):
     if request.method == 'DELETE':
         try:
-            retrive_obj  = NoteModel.objects.get(id=id)
+            retrive_obj = NoteModel.objects.get(id=id, user=request.user)
             retrive_obj.delete()
-            return Response({'msg':' Note Deleted successfully.'}, status=status.HTTP_200_OK)
+            return Response({'msg': ' Note Deleted successfully.'}, status=status.HTTP_200_OK)
         except NoteModel.DoesNotExist:
-            return Response({'msg':'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'msg':'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
-
-
+            return Response({'msg': 'Given Id does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'msg': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
