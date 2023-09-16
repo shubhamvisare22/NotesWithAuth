18e0c50a916a22385285f6bf86404bffc0364bc4
shubhamvisare22 <shubhamvisare22@gmail.com>
2023-09-17 02:29:30 +0530

    added documentation

--- NoteTask/noteapp/urls.py
+++ NoteTask/noteapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('create-note', views.create_note, name='create'),
-    path('get-note', views.list_notes, name='get'),
-    path('get-note/<int:id>', views.retrieve_note, name='retrieve'),
+    path('list-note', views.list_notes, name='list'),
+    path('retrieve-note/<int:id>', views.retrieve_note, name='retrieve'),
     path('update-note/<int:id>', views.update_note, name='update'),
     path('patch-note/<int:id>', views.patch_note, name='patch'),
     path('delete-note/<int:id>', views.delete_note, name='delete'),
