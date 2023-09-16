2381733a6f63a9702ad70f06eedf3cccf3945d32
shubhamvisare22 <shubhamvisare22@gmail.com>
2023-09-16 18:20:58 +0530

    updated user spicific notes

--- NoteTask/NoteTask/settings.py
+++ NoteTask/NoteTask/settings.py
@@ -10,8 +10,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -19,18 +17,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
-# Externally Added apps 
+# Externally Added apps
 
 INSTALLED_APPS += [
-    
+
     'noteapp',
     'rest_framework',
     'rest_framework.authtoken',
-    
-    ]
+
+]
 
 
 MIDDLEWARE = [
@@ -64,9 +62,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'NoteTask.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -74,9 +69,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -94,9 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -106,22 +95,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    
+
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 2,
-    
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
--- NoteTask/noteapp/models.py
+++ NoteTask/noteapp/models.py
@@ -2,12 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class NoteModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
-    
-    
+    updated_at = models.DateTimeField(auto_now=True)
